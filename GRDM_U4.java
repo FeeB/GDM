dea48934bfa37ace45d782000ae0769419c8302f
Fee <fee.braun@hotmail.com>
2012-11-28 17:04:30 +0100

    Schiebe Blende und Weiche Blende

--- GRDM_U4.java
+++ GRDM_U4.java
@@ -6,23 +6,23 @@ import ij.gui.*;
 import java.awt.*;
 import ij.plugin.filter.*;
 
-
 public class GRDM_U4 implements PlugInFilter {
 
 	protected ImagePlus imp;
-	final static String[] choices = {"Wischen", "Weiche Blende", "Chroma Key", "Extra"};
+	final static String[] choices = { "Wischen", "Weiche Blende", "Overlay", "Schieb Blende", "Chroma Key",
+			"Extra" };
 
 	public int setup(String arg, ImagePlus imp) {
 		this.imp = imp;
-		return DOES_RGB+STACK_REQUIRED;
+		return DOES_RGB + STACK_REQUIRED;
 	}
-	
+
 	public static void main(String args[]) {
-		ImageJ ij = new ImageJ(); // neue ImageJ Instanz starten und anzeigen 
+		ImageJ ij = new ImageJ(); // neue ImageJ Instanz starten und anzeigen
 		ij.exitWhenQuitting(true);
-		
+
 		IJ.open("/Applications/ImageJ/Bilder/StackB.zip");
-		
+
 		GRDM_U4 sd = new GRDM_U4();
 		sd.imp = IJ.getImage();
 		ImageProcessor B_ip = sd.imp.getProcessor();
@@ -32,48 +32,58 @@ public class GRDM_U4 implements PlugInFilter {
 	public void run(ImageProcessor B_ip) {
 		// Film B wird uebergeben
 		ImageStack stack_B = imp.getStack();
-		
+
 		int length = stack_B.getSize();
-		int width  = B_ip.getWidth();
+		int width = B_ip.getWidth();
 		int height = B_ip.getHeight();
-		
+
 		// ermoeglicht das Laden eines Bildes / Films
 		Opener o = new Opener();
-		OpenDialog od_A = new OpenDialog("Auswählen des 2. Filmes ...",  "");
-				
+		OpenDialog od_A = new OpenDialog("Auswählen des 2. Filmes ...", "");
+
 		// Film A wird dazugeladen
 		String dateiA = od_A.getFileName();
-		if (dateiA == null) return; // Abbruch
+		if (dateiA == null)
+			return; // Abbruch
 		String pfadA = od_A.getDirectory();
-		ImagePlus A = o.openImage(pfadA,dateiA);
-		if (A == null) return; // Abbruch
+		ImagePlus A = o.openImage(pfadA, dateiA);
+		if (A == null)
+			return; // Abbruch
 
 		ImageProcessor A_ip = A.getProcessor();
-		ImageStack stack_A  = A.getStack();
+		ImageStack stack_A = A.getStack();
 
-		if (A_ip.getWidth() != width || A_ip.getHeight() != height)
-		{
+		if (A_ip.getWidth() != width || A_ip.getHeight() != height) {
 			IJ.showMessage("Fehler", "Bildgrößen passen nicht zusammen");
 			return;
 		}
-		
-		// Neuen Film (Stack) "Erg" mit der kleineren Laenge von beiden erzeugen
-		length = Math.min(length,stack_A.getSize());
 
-		ImagePlus Erg = NewImage.createRGBImage("Ergebnis", width, height, length, NewImage.FILL_BLACK);
-		ImageStack stack_Erg  = Erg.getStack();
+		// Neuen Film (Stack) "Erg" mit der kleineren Laenge von beiden erzeugen
+		length = Math.min(length, stack_A.getSize());
+
+		ImagePlus Erg = NewImage.createRGBImage("Ergebnis", width, height,
+				length, NewImage.FILL_BLACK);
+		ImageStack stack_Erg = Erg.getStack();
 
 		// Dialog fuer Auswahl des Ueberlagerungsmodus
 		GenericDialog gd = new GenericDialog("Überlagerung");
-		gd.addChoice("Methode",choices,"");
+		gd.addChoice("Methode", choices, "");
 		gd.showDialog();
 
-		int methode = 0;		
+		int methode = 0;
 		String s = gd.getNextChoice();
-		if (s.equals("Wischen")) methode = 1;
-		if (s.equals("Weiche Blende")) methode = 2;
-		if (s.equals("Chroma Key")) methode = 3;
-		if (s.equals("Extra")) methode = 4;
+		if (s.equals("Wischen"))
+			methode = 1;
+		if (s.equals("Weiche Blende"))
+			methode = 2;
+		if (s.equals("Overlay"))
+			methode = 3;
+		if (s.equals("Schieb Blende"))
+			methode = 4;
+		if (s.equals("Croma Keying"))
+			methode = 5;
+		if (s.equals("Extra"))
+			methode = 6;
 
 		// Arrays fuer die einzelnen Bilder
 		int[] pixels_B;
@@ -81,17 +91,16 @@ public class GRDM_U4 implements PlugInFilter {
 		int[] pixels_Erg;
 
 		// Schleife ueber alle Bilder
-		for (int z=1; z<=length; z++)
-		{
-			pixels_B   = (int[]) stack_B.getPixels(z);
-			pixels_A   = (int[]) stack_A.getPixels(z);
+		for (int z = 1; z <= length; z++) {
+			pixels_B = (int[]) stack_B.getPixels(z);
+			pixels_A = (int[]) stack_A.getPixels(z);
 			pixels_Erg = (int[]) stack_Erg.getPixels(z);
 
 			int pos = 0;
-			for (int y=0; y<height; y++)
-				for (int x=0; x<width; x++, pos++)
-				{
+			for (int y = 0; y < height; y++)
+				for (int x = 0; x < width; x++, pos++) {
 					int cA = pixels_A[pos];
+					int aA = (cA & 0xff000000) >> 24;
 					int rA = (cA & 0xff0000) >> 16;
 					int gA = (cA & 0x00ff00) >> 8;
 					int bA = (cA & 0x0000ff);
@@ -101,27 +110,40 @@ public class GRDM_U4 implements PlugInFilter {
 					int gB = (cB & 0x00ff00) >> 8;
 					int bB = (cB & 0x0000ff);
 
-					if (methode == 1)
-					{
-					if (x+1 > (z-1)*(double)width/(length-1))
-						pixels_Erg[pos] = pixels_B[pos];
-					else
-						pixels_Erg[pos] = pixels_A[pos];
+					if (methode == 1) {
+						if (y + 1 > (z - 1) * (double) height / (length - 1))
+							pixels_Erg[pos] = pixels_B[pos];
+						else
+							pixels_Erg[pos] = pixels_A[pos];
 					}
 
-					/*
-					if (methode == 2)
-					{
-					// ...
+					if (methode == 2) {
+						float trans = 255f / (length - 1) * (z - 1); 
+						float dif = 255f - trans;
+
+						int r = (int) ((trans * rA + dif * rB) / 255f);
+						int b = (int) ((trans * bA + dif * bB) / 255f);
+						int g = (int) ((trans * gA + dif * gB) / 255f);
+
+						pixels_Erg[pos] = 0xFF000000 + ((r & 0xff) << 16) + ((g & 0xff) << 8) + (b & 0xff);
+					}
 					
-					int r = ...
-					int g = ...
-					int b = ...
-
-					pixels_Erg[pos] = 0xFF000000 + ((r & 0xff) << 16) + ((g & 0xff) << 8) + ( b & 0xff);
+					if (methode == 4) {
+						
+						if (x + 1 > (z - 1) * (double) width / (length - 1)) {
+							int posNew = (int) (pos - ((z - 1) * (double) width / (length - 1)));
+							pixels_Erg[pos] = pixels_B[posNew];
+						}else {
+							int posNew = (int) (pos - ((z - 1) * (double) width / (length - 1)));
+							if (posNew < 0){
+								posNew = 0;
+							}
+							pixels_Erg[pos] = pixels_A[posNew];
+						}
 					}
-					*/
+
 				}
+
 		}
 
 		// neues Bild anzeigen
@@ -129,6 +151,4 @@ public class GRDM_U4 implements PlugInFilter {
 		Erg.updateAndDraw();
 
 	}
-
 }
-
